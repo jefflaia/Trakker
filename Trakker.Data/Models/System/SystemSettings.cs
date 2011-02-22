6f34d9784363b7c6e41354072aad8c51910bbaff
Justin <justin.arvay@gmail.com>
2011-02-22 00:00:00 -0800

    - added system settings model

---
+++ Trakker.Data/Models/System/SystemSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Trakker.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+
+    public class SystemSettings
+    {
+        public ColorPalette ColorPalette { get; set; }
+        public String LogoUrl { get; set; }
+        public String TimeFormat { get; set; }
+        public String DayFormat { get; set; }
+        public String DateTimeFormat { get; set; }
+        public String DateFormat { get; set; }
+
+    }
+}
